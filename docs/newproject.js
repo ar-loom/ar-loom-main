e84f5b65c7a36639ca10cd3cbb982b7f56825982
ar-loom <161101994+ar-loom@users.noreply.github.com>
2024-04-03 00:37:47 -0700

    Update newproject.js

--- docs/newproject.js
+++ docs/newproject.js
@@ -1,21 +1,4 @@
- function attachButtonListeners() {
-        var tablinks = document.getElementsByClassName("tablinks");
-        for (let i = 0; i < tablinks.length; i++) {
-            tablinks[i].addEventListener('click', function(evt) {
-                var cityName = this.getAttribute("data-city"); // Assuming you use a data attribute to store the city name
-                openCity(cityName);
-                evt.preventDefault(); // Prevent default button behavior
-            });
-        }
-    }
-    
-
-    // Prevent clicks on the tab from triggering the reset
-    document.querySelector(".tab").addEventListener("click", function(event) {
-        event.stopPropagation();
-    });
-
-    // Setup IntersectionObserver for fade-in effects
+ // Setup IntersectionObserver for fade-in effects
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
